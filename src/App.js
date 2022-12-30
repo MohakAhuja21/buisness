35fabbcd578a0460e20bbbcedbc2bc19b9ff3bb2
Mohak Ahuja <mohakaahuaj@outlook.com>
2022-12-30 22:51:39 +0530

    npm terser installed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './components/Home'
 import Login from './components/Login'
 import SignUp from './components/SignUp'
-import PgFOF from './components/PgFOF.JS'
 import Cart from "./components/Cart";
 
 function App() {
@@ -15,8 +14,6 @@ function App() {
 <Route exact path ="/home" element={<Home/>}/>
 <Route exact path ="/cart" element={<Cart/>}/>
 <Route exact path ="/signup" element={<SignUp/>}/>
-{/* page not found is always kept at the last. */}
-<Route path ="*" element={<PgFOF/>}/>
   </Routes>
   </BrowserRouter>
   );
